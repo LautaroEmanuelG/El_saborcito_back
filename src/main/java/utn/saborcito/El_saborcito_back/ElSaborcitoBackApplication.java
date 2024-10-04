6280c6c5be3b4aafe91c005143a13ad648a41d1f
LautaroEmanuelG <lgonzalez.sag98@gmail.com>
2024-10-04 09:48:37 -0300

    Feat (Project) : Initial Project

---
+++ src/main/java/utn/saborcito/El_saborcito_back/ElSaborcitoBackApplication.java
@@ -0,0 +1,13 @@
+package utn.saborcito.El_saborcito_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElSaborcitoBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElSaborcitoBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/utn/saborcito/El_saborcito_back/ElSaborcitoBackApplicationTests.java
@@ -0,0 +1,13 @@
+package utn.saborcito.El_saborcito_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElSaborcitoBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
