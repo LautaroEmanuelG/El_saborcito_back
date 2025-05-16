62cee47fbc576437b8a4d46991b050637a68a18b
ErosMariotti <145143267+ErosMariotti@users.noreply.github.com>
2025-05-16 20:44:27 -0300

    Endpoints: errores, feedback y control: Correccion 23

--- src/main/java/utn/saborcito/El_saborcito_back/services/ArticuloService.java
+++ src/main/java/utn/saborcito/El_saborcito_back/services/ArticuloService.java
@@ -4,10 +4,12 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
+import utn.saborcito.El_saborcito_back.dto.ArticuloDTO;
 import utn.saborcito.El_saborcito_back.models.Articulo;
 import utn.saborcito.El_saborcito_back.repositories.ArticuloRepository;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -15,14 +17,28 @@ public class ArticuloService {
 
     private final ArticuloRepository repo;
 
-    public List<Articulo> findAll() {
-        return repo.findAll();
-    }
 
     public Articulo findById(Long id) {
         return repo.findById(id)
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Artículo no encontrado"));
     }
+
+    public List<ArticuloDTO> findAll() {
+        return repo.findAll().stream()
+                .map(this::toDto)
+                .collect(Collectors.toList());
+    }
+
+    private ArticuloDTO toDto(Articulo articulo) {
+        ArticuloDTO dto = new ArticuloDTO();
+        dto.setId(articulo.getId());
+        dto.setDenominacion(articulo.getDenominacion());
+        dto.setPrecioVenta(articulo.getPrecioVenta());
+        dto.setCategoriaId(articulo.getCategoria().getId());
+        dto.setUnidadMedidaId(articulo.getUnidadMedida().getId());
+        return dto;
+    }
+
 
     public Articulo save(Articulo articulo) {
         return repo.save(articulo);
