bf362856c0134241b2c1a9592ce9379b761ee286
ErosMariotti <145143267+ErosMariotti@users.noreply.github.com>
2025-05-16 20:04:33 -0300

    Relaciones mal diseñadas: Correccion 8

--- src/main/java/utn/saborcito/El_saborcito_back/models/Sucursal.java
+++ src/main/java/utn/saborcito/El_saborcito_back/models/Sucursal.java
@@ -17,8 +17,6 @@ public class Sucursal {
     private Long id;
 
     private String nombre;
-    private LocalTime horarioApertura;
-    private LocalTime horarioCierre;
 
     // Uno a uno con Domicilio
     @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
@@ -30,4 +28,6 @@ public class Sucursal {
     @JoinColumn(name = "empresa_id", nullable = false)
     private Empresa empresa;
 
+    @OneToMany(mappedBy = "sucursal", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<HorarioAtencion> horarios;
 }
