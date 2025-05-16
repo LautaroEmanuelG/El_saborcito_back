123a58f2e8441167664e0e216594a11e1670c8b2
ErosMariotti <145143267+ErosMariotti@users.noreply.github.com>
2025-05-16 19:56:52 -0300

    eliminacion de clases viejas

--- src/main/java/utn/saborcito/El_saborcito_back/repositories/TransaccionRepository.java
+++
@@ -1,8 +0,0 @@
-package utn.saborcito.El_saborcito_back.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TransaccionRepository extends JpaRepository<Transaccion, Long> {
-}
