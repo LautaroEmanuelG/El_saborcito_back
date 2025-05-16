06a8c329c942eeb0db7849e10eef6d65303b461a
ErosMariotti <145143267+ErosMariotti@users.noreply.github.com>
2025-05-16 20:50:46 -0300

    Problemas técnicos con Hibernate / JPA: Correccion 25

--- src/main/java/utn/saborcito/El_saborcito_back/services/SucursalService.java
+++ src/main/java/utn/saborcito/El_saborcito_back/services/SucursalService.java
@@ -1,20 +1,22 @@
 package utn.saborcito.El_saborcito_back.services;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
+import utn.saborcito.El_saborcito_back.models.Domicilio;
 import utn.saborcito.El_saborcito_back.models.Sucursal;
+import utn.saborcito.El_saborcito_back.repositories.DomicilioRepository;
 import utn.saborcito.El_saborcito_back.repositories.SucursalRepository;
 
 import java.util.List;
+@RequiredArgsConstructor
 
 @Service
 public class SucursalService {
     private final SucursalRepository repo;
+    private final DomicilioRepository domicilioRepository;
 
-    public SucursalService(SucursalRepository repo) {
-        this.repo = repo;
-    }
 
     public List<Sucursal> findAll() {
         return repo.findAll();
@@ -30,13 +32,21 @@ public class SucursalService {
     }
 
     public Sucursal update(Long id, Sucursal dto) {
+        if (dto.getDomicilio() != null && dto.getDomicilio().getId() != null) {
+            Domicilio domicilioExistente = domicilioRepository.findById(dto.getDomicilio().getId())
+                    .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Domicilio no encontrado"));
+            dto.setDomicilio(domicilioExistente);
+        }
+
         Sucursal existing = findById(id);
         existing.setNombre(dto.getNombre());
         existing.setHorarios(dto.getHorarios());
         existing.setDomicilio(dto.getDomicilio());
         existing.setEmpresa(dto.getEmpresa());
+
         return repo.save(existing);
     }
+
 
     public void delete(Long id) {
         if (!repo.existsById(id))
