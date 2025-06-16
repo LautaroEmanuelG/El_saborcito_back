821bbbe0449aee3ae4a07cce06ad125bd42f7d1d
LautaroEmanuelG <lgonzalez.sag98@gmail.com>
2025-06-16 16:50:18 -0300

    📦 Feat (HistorialPedido): Añadir DTO y mapper para optimizar el historial de pedidos, implementar método optimizado en el servicio, agregar consulta optimizada en el repositorio, incluir nueva ruta en el controlador, actualizar configuración de Cloudinary.

--- src/main/java/utn/saborcito/El_saborcito_back/repositories/PedidoRepository.java
+++ src/main/java/utn/saborcito/El_saborcito_back/repositories/PedidoRepository.java
@@ -11,10 +11,13 @@ import java.util.List;
 @Repository
 
 public interface PedidoRepository extends JpaRepository<Pedido, Long> {
-        
+
     List<Pedido> findAllByFechaPedidoBetween(LocalDate desde, LocalDate hasta);
+
     List<Pedido> findAllByCliente_IdAndFechaPedidoBetween(Long clienteId, LocalDate desde, LocalDate hasta);
+
     List<Pedido> findByEstadoNombreIn(List<String> nombres);
 
+    List<Pedido> findByClienteId(Long clienteId);
 
 }
